57b177f01926f95b2734280eb2d9014fd346a493
Juli Kosulnikova <julkakos@mail.ru>
2024-04-17 00:30:06 +0300

    исправление замечаний

--- src/main/java/ru/juli/practicum/courier/CourierGenerator.java
+++ src/main/java/ru/juli/practicum/courier/CourierGenerator.java
@@ -16,19 +16,3 @@ public class CourierGenerator {
         return new Courier(login, password, firstName);
     }
 }
-
-
-//public class CourierGenerator {
-//
-//    public static void main(String[] args) {
-//        Faker faker = new Faker();
-//
-//        String login = faker.name().username(); // Генерация логина курьера
-//        String password = faker.internet().password(4, 8, true, true); // Генерация пароля
-//        String firstName = faker.name().firstName(); // Генерация имени курьера
-//
-//        System.out.println("Login: " + login);
-//        System.out.println("Password: " + password);
-//        System.out.println("FirstName: " + firstName);
-//    }
-//}
